87aaf7bfa549e5f41a3e92c473d94d689a9e8577
abdulaziz20031942101 <150109615+abdulaziz20031942101@users.noreply.github.com>
2024-03-15 12:11:58 +0500

    header section

---
+++ src/component/Navbar.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import { HiMiniBars2 } from "react-icons/hi2";
+
+const Navbar = () => {
+    
+    const [toggle,setToggle] = useState(false)
+ 
+  return (
+
+   
+    <nav className=' containerr fixed w-full h-[80px] backdrop-blur flex justify-between items-center'>
+        <div className='text-[30px] font-bold'>Abdulaziz.</div>
+        <div>
+            <ul className={`flex md:flex-row flex-col text-[20px] gap-10 text-[#6b7688]  font-[600] uppercase md:static  bg-[white] absolute md:w-aoto md:bg-transparent w-[80%] ${toggle? 'right-0': 'right-[-100%]'} top-[80px] md:h-auto h-[100vh] transition-[1s] md:p-0 p-10`}>
+                <li><a href="#"  className='links'>Home</a></li>
+                <li><a href="#"  className='links'>Skills</a></li>
+                <li><a href="#"  className='links'>Works</a></li>
+                <li><a href="#" className='links'>Contact</a></li>
+            </ul>
+        </div>
+        <div className='flex gap-5'>
+            <div>RU</div>
+            <div>ENG</div>
+        </div>
+
+        {/* bars  */}
+        <div className=' p-1 bg-[#313bac] text-[white] rounded-full text-[20px] md:hidden block' onClick={()=>setToggle(!toggle)}>
+        <HiMiniBars2 />
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ src/pages/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+// companints 
+import Navbar from "../component/Navbar"
+import Header from '../component/Header'
+
+const main = () => {
+  return (
+    <div>
+        <Navbar/>
+        <Header/>
+    </div>
+  )
+}
+
+export default main
